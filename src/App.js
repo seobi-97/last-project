9cfef952b5cbe2c54607b7efa92cb5c6400ec044
seobi-97 <tkfkdrk12@skuniv.ac.kr>
2022-03-17 21:51:16 +0900

    작업 환경 설정

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import LoginPage from './LoginPage/LoginPage';
+import RegisterPage from './RegisterPage/RegisterPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route exact path="/LoginPage" element={<LoginPage/>}/>
+          <Route exact path="/RegisterPage" element={<RegisterPage/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
