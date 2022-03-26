8049c8046579e8c7e434048c77273f6281820c7f
seobi-97 <tkfkdrk12@skuniv.ac.kr>
2022-03-27 02:33:16 +0900

    react-hook-form version6->7

--- src/RegisterPage/RegisterPage.js
+++ src/RegisterPage/RegisterPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useform } from 'react-hook-form';
 function RegisterPage() {
-  const { register, watch, errors } = useForm({mode: "onChange"});
+  const { register, watch, formState: {errors},handleSubmit } = useForm();
   const password = useRef();
   password.current = watch("password");
 
@@ -14,23 +14,23 @@ function RegisterPage() {
             </div>
             <form>
                 <label>Email</label>
-                <input name="email" type="email" ref={register({required: true, pattern: /^\S+@\S+$/i})}/>
+                <input name="email" type="email" {...register({required: true, pattern: /^\S+@\S+$/i})}/>
                 {errors.email && <p>This email field is required</p>}
                 
                 <label>Name</label>
-                <input name="name" ref={register({required:true, maxLength:10})}/>
+                <input name="name" {...register({required:true, maxLength:10})}/>
                 {errors.name && errors.name.type === "required"
                   && <p>This name field is required</p>}
                 {errors.name && errors.name.type === "maxLength"
                   && <p>Your input exceed maxinum length</p>}
                 
                 <label>Password</label>
-                <input name="password" type="password" ref={register({required:true, pattern: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,16}$/})}/>
+                <input name="password" type="password" {...register({required:true, pattern: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,16}$/})}/>
                 {errors.password && errors.password.type === "required" && <p>This name field is required</p>}
                 {errors.password && errors.password.type === "pattern" && <p>최소 8자에서 최대 16자, 최소 하나의 문자, 하나의 숫자 및 하나의 특수 문자 사용</p>}
                 
                 <label>Password Confirm</label>
-                <input name="password_confirm" type="password" ref={register({required:true, validate:(value)=>value===password.current})}/>
+                <input name="password_confirm" type="password" {...register({required:true, validate:(value)=>value===password.current})}/>
                 {errors.password_confirm && errors.password_confirm.type === "required" && <p>This password confirm field is required</p>}
                 {errors.password_confirm && errors.password_confirm.type === "validate" && <p>The passwords do not match</p>}
                 <input type="submit"/>
